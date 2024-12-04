5948accd4c134d9f75ee51b7aeac63ce60c35e77
Ethan Blanche <blancheq@bc.edu>
2024-12-04 11:20:11 -0500

    Initial Commit

---
+++ HarryPotterArchive/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HarryPotterArchive
+//
+//  Created by Ethan Blanche on 12/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HarryPotterArchive/HarryPotterArchiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterArchiveApp.swift
+//  HarryPotterArchive
+//
+//  Created by Ethan Blanche on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct HarryPotterArchiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
